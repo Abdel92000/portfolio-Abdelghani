1a0aeef4062cbdc1d6710d9d8c4a1b2d942b5f95
Abdel92000 <abdelghani.rahal123@gmail.com>
2025-05-22 13:03:17 +0200

    forçage de redeploy pour mise à jour ENV

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
-interface ImportMetaEnv {
-  readonly VITE_EMAILJS_SERVICE_ID: string;
-  readonly VITE_EMAILJS_TEMPLATE_ID: string;
-  readonly VITE_EMAILJS_PUBLIC_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+/// <reference types="vite/client" />
